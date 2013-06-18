adf1f3cb38bcb0ad12770df64ef09aca0764c0ce
Zeze Lazo <zezelazo@gmail.com>
2013-06-18 17:47:39 -0500

    adding basic references

--- isct.arduinoscada.web/App_Start/BundleConfig.cs
+++ isct.arduinoscada.web/App_Start/BundleConfig.cs
@@ -1,14 +1,39 @@
-﻿using System.Web.Optimization;
+﻿using System;
+using System.Web.Optimization;
 
 namespace isct.arduinoscada.web.App_Start {
     public class BundleConfig {
         public static void RegisterBundles(BundleCollection bundles) {
             bundles.IgnoreList.Clear();
             AddDefaultIgnorePatterns(bundles.IgnoreList);
+
+            bundles.Add(new ScriptBundle("~/scripts/modernizr")
+                .Include("~/scripts/modernizr-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts/vendor")
+                .Include("~/scripts/jquery-{version}.min.js")
+                .Include("~/scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/ie10mobile.css")
+                .Include("~/Content/bootstrap.min.css")
+                .Include("~/Content/bootstrap-responsive.min.css")
+                .Include("~/Content/font-awesome.min.css"));
+
         }
 
-        private static void AddDefaultIgnorePatterns(IgnoreList ignoreList) {
-            throw new System.NotImplementedException();
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            if (ignoreList == null)
+            {
+                throw new ArgumentNullException("ignoreList");
+            }
+
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            //ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
         }
     }
 }
---
+++ isct.arduinoscada.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="isct.arduinoscada.web.Global" Language="C#" %>
